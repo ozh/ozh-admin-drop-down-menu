4b461670ea13db0621b24f209be43214d3c93a86
ozh <ozh@ozh.org>
2009-09-20 09:20:20 +0000

    3.3.4: Added: ro_RO (thanks Octav!) Added: uk_UA (thanks wpp.pp.ua)
    git-svn-id: http://plugins.svn.wordpress.org/ozh-admin-drop-down-menu/trunk@156413 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_ozh_adminmenu.php
+++ wp_ozh_adminmenu.php
@@ -3,7 +3,7 @@
 Plugin Name: Ozh' Admin Drop Down Menu
 Plugin URI: http://planetozh.com/blog/my-projects/wordpress-admin-menu-drop-down-css/
 Description: All admin links available in a neat horizontal drop down menu. Saves lots of screen real estate! <strong>For WordPress 2.8+</strong>
-Version: 3.3.3
+Version: 3.3.4
 Author: Ozh
 Author URI: http://planetOzh.com/
 */
@@ -84,6 +84,9 @@ Author URI: http://planetOzh.com/
               Fixed: silly toggling of the "Display Favorites" option (thanks johnbillion!)
 			  Improved: display with crappy IE8 (thanks Octav!)
  * 3.3.3:     Added: he_IL (thanks Amiad Bareli!)
+ * 3.3.4:     Added: ro_RO (thanks Octav!)
+              Added: uk_UA (thanks wpp.pp.ua)
+
  */
 
 /***** Hook things in when visiting an admin page. When viewing a blog page, nothing even loads in memory. ****/
