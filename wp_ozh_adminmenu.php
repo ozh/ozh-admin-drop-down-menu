95a8bcb667cdfe586e5d2c7eda950ec038088550
ozh <ozh@ozh.org>
2010-03-07 16:50:30 +0000

    3.3.8 Added: zh_TW (thanks Paogray !)
    git-svn-id: http://plugins.svn.wordpress.org/ozh-admin-drop-down-menu/trunk@214552 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_ozh_adminmenu.php
+++ wp_ozh_adminmenu.php
@@ -3,7 +3,7 @@
 Plugin Name: Ozh' Admin Drop Down Menu
 Plugin URI: http://planetozh.com/blog/my-projects/wordpress-admin-menu-drop-down-css/
 Description: All admin links available in a neat horizontal drop down menu. Saves lots of screen real estate! <strong>For WordPress 2.8+</strong>
-Version: 3.3.7
+Version: 3.3.8
 Author: Ozh
 Author URI: http://planetOzh.com/
 */
@@ -89,6 +89,7 @@ Author URI: http://planetOzh.com/
  * 3.3.5:     Added: uk (thanks Jurko Chervony!)
  * 3.3.6:     Minor cosmetic change to backend (twitter links)
  * 3.3.7:     Fixed: javascript error occurence (thanks Jaume !)
+ * 3.3.8:     Added: zh_TW (thanks Paogray !)
 
  */
 
